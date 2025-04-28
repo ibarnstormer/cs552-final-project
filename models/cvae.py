dfc6d1d97174ef44a0674187d14c50e9e7b72617
ibarnstormer <48729319+ibarnstormer@users.noreply.github.com>
2025-04-28 18:16:33 -0400

    CVAE model

--- models/cvae.py
+++ models/cvae.py
@@ -3,9 +3,112 @@ Convolutional VAE
 
 """
 
+import numpy as np
+import torch
 import torch.nn as nn
 
 
+class CVAEEncoder(nn.Module):
+
+    def __init__(self, latent_dim: int, input_dim: int, input_channels: int):
+        super(CVAEEncoder, self).__init__()
+
+        ks = input_dim ** 0.5
+
+        # (Input dim + padding - kernel dim) / stride + 1
+        for _ in range(0, 2):
+          ks = int(np.floor((ks + 2 - 4) / 2) + 1)
+
+        self.encoder = nn.Sequential(
+            nn.Conv2d(in_channels=input_channels, kernel_size=(4, 4), stride=2, padding=1, out_channels=16),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=16, kernel_size=(4, 4), stride=2, padding=1, out_channels=32),
+            nn.ReLU(),
+            nn.Flatten()
+        )
+
+        self.fc_mu = nn.Linear(in_features=32 * ks ** 2, out_features=latent_dim)
+        self.fc_logvar = nn.Linear(in_features=32 * ks ** 2, out_features=latent_dim)
+
+    def forward(self, x):
+        z = self.encoder(x)
+        mu = self.fc_mu(z)
+        logvar = self.fc_logvar(z)
+
+        return mu, logvar
+
+
+class CVAEDecoder(nn.Module):
+
+    def __init__(self, latent_dim: int, input_dim: int, output_channels: int):
+        super(CVAEDecoder, self).__init__()
+
+        ks = input_dim ** 0.5
+        
+        # (Input dim + padding - kernel dim) / stride + 1
+        for _ in range(0, 2):
+          ks = int(np.floor((ks + 2 - 4) / 2) + 1)
+
+        self.decoder = nn.Sequential(
+            nn.Linear(in_features=latent_dim, out_features=32 * ks ** 2),
+            nn.ReLU(),
+            nn.Unflatten(dim=1, unflattened_size=(32, ks, ks)),
+            nn.ConvTranspose2d(in_channels=32, kernel_size=(4, 4), stride=2, padding=1, out_channels=16),
+            nn.ReLU(),
+            nn.ConvTranspose2d(in_channels=16, kernel_size=(4, 4), stride=2, padding=1, out_channels=output_channels),
+            nn.Sigmoid()
+        )
+    
+    def forward(self, z):
+        return self.decoder(z)
+
+
 class CVAE(nn.Module):
-    # TODO
-    pass
+    """
+    Convolutional Variational Autoencoder
+
+    This network uses convolutions for the encoder 
+    and transpose convolutions + linear for the decoder
+    """
+    
+    def __init__(self, device, latent_dim: int, input_dim: int, input_channels: int = 3):
+        super(CVAE, self).__init__()
+        self.latent_dim = latent_dim
+        self.device = device
+
+        self.encoder = CVAEEncoder(latent_dim=latent_dim, input_dim=input_dim, input_channels=input_channels)
+        self.decoder = CVAEDecoder(latent_dim=latent_dim, input_dim=input_dim, output_channels=input_channels)
+
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(std)
+        return eps * std + mu
+    
+    def forward(self, x):
+        mu, logvar = self.encoder(x)
+        z = self.reparameterize(mu, logvar)
+        x_recon = self.decoder(z)
+        return x_recon, mu, logvar
+    
+    def encode(self, x):
+        mu, logvar = self.encoder(x)
+        return self.reparameterize(mu, logvar)
+    
+    def generate(self, n: int = 1):
+        z = torch.randn((n, self.latent_dim)).to(self.device)
+        x_recon = self.decoder(z)
+        return x_recon
+    
+    @staticmethod
+    def vae_loss(output, x):
+        """
+        KL-Divergence loss with BCE
+
+        output is a tuple of the following: (reconstructed x, mu/mean, log variance)
+        """
+        b = 0.25
+        x_recon, mu, logvar = output
+
+        loss = nn.functional.binary_cross_entropy(x_recon, x, reduction="sum")
+        kl_divergence = -b * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+        return loss + kl_divergence
